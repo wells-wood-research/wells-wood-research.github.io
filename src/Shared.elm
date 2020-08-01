830aef9118f731b1dacc66a3fdc8b21434839bb0
Chris Wells Wood <cwwoodesq@gmail.com>
2020-08-01 10:16:41 +0100

    Feat: Adds Msg and Sub required for responsive pages.

--- src/Shared.elm
+++ src/Shared.elm
@@ -8,11 +8,14 @@ module Shared exposing
     , view
     )
 
+import Browser.Dom
+import Browser.Events
 import Browser.Navigation exposing (Key)
 import Element exposing (..)
 import Element.Font as Font
 import Spa.Document exposing (Document)
 import Spa.Generated.Route as Route
+import Task
 import Url exposing (Url)
 
 
@@ -27,13 +30,23 @@ type alias Flags =
 type alias Model =
     { url : Url
     , key : Key
+    , mDevice : Maybe Device
     }
 
 
 init : Flags -> Url -> Key -> ( Model, Cmd Msg )
-init flags url key =
-    ( Model url key
-    , Cmd.none
+init _ url key =
+    ( { url = url
+      , key = key
+      , mDevice = Nothing
+      }
+    , Task.perform
+        (\viewport ->
+            PageResized
+                (floor viewport.scene.width)
+                (floor viewport.scene.height)
+        )
+        Browser.Dom.getViewport
     )
 
 
@@ -42,19 +55,26 @@ init flags url key =
 
 
 type Msg
-    = ReplaceMe
+    = PageResized Int Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ReplaceMe ->
-            ( model, Cmd.none )
+        PageResized width height ->
+            ( { model
+                | mDevice =
+                    Just <|
+                        classifyDevice
+                            { width = width, height = height }
+              }
+            , Cmd.none
+            )
 
 
 subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
+subscriptions _ =
+    Browser.Events.onResize PageResized
 
 
 
@@ -65,13 +85,15 @@ view :
     { page : Document msg, toMsg : Msg -> msg }
     -> Model
     -> Document msg
-view { page, toMsg } model =
+view { page } _ =
     { title = page.title
     , body =
         [ column [ padding 20, spacing 20, height fill ]
             [ row [ spacing 20 ]
-                [ link [ Font.color (rgb 0 0.25 0.5), Font.underline ] { url = Route.toString Route.Top, label = text "Homepage" }
-                , link [ Font.color (rgb 0 0.25 0.5), Font.underline ] { url = Route.toString Route.NotFound, label = text "Not found" }
+                [ link [ Font.color (rgb 0 0.25 0.5), Font.underline ]
+                    { url = Route.toString Route.Top, label = text "Homepage" }
+                , link [ Font.color (rgb 0 0.25 0.5), Font.underline ]
+                    { url = Route.toString Route.NotFound, label = text "Not found" }
                 ]
             , column [ height fill ] page.body
             ]
